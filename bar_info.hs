d6984a35ba93609d27411a35f4ca7313edaa3d17
Mabry Cervin <mpcervin@uncg.edu>
2015-01-18 15:57:24 -0500

    Added battery level and charging status

--- bar_info.hs
+++ bar_info.hs
@@ -7,6 +7,7 @@ import Text.ParserCombinators.Parsec
 import Data.Time
 default (T.Text)
 
+-- Parses the output of getZonedTime to "yyyy-mm-dd hh-mm-ss"
 clockParser :: String -> Either ParseError String
 clockParser = parse beforeDot "failure"
     where
@@ -14,9 +15,21 @@ clockParser = parse beforeDot "failure"
 
 
 main :: IO ()
-main = do
-        time <- getZonedTime
+main = shelly $ verbosely $ do
+        time <- liftIO getZonedTime
         let timestring = case (clockParser $ show time) of
                              Right str -> str
                              Left _ -> ""
-        putStrLn timestring
+        echo $ pack timestring
+
+        batLvl <- readfile "/sys/class/power_supply/BAT0/capacity"
+        let batLevel = strip batLvl
+        echo batLevel
+
+        batChrg <- readfile "/sys/class/power_supply/BAT0/status"
+        let batCharging = case (unpack $ strip batChrg) of
+                           "Unknown" -> True
+                           "Discharging" -> False
+                           _ -> False
+        echo $ pack $ show batCharging
+
