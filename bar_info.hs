ae9d05d0964520c4b394710ddf2d833d1b89eb34
Mabry Cervin <mpcervin@uncg.edu>
2015-01-25 15:20:38 -0500

    Got everything working, but requires the util unbuffer to work with bar

--- bar_info.hs
+++ bar_info.hs
@@ -6,6 +6,7 @@ import Data.Text as T hiding (map, tail)
 import Text.ParserCombinators.Parsec
 import Text.ParserCombinators.Parsec.Token as P
 import Data.Time
+import Control.Monad (forever)
 default (T.Text)
 
 -- Parses the output of getZonedTime to "yyyy-mm-dd hh:mm:ss"
@@ -40,16 +41,16 @@ psOutSum :: Text -> Int
 psOutSum = round . sum . str2doubles . map unpack . tail . map strip . T.lines
 
 buildDeskText :: (Text, Text, Text, Text) -> Int -> Int -> Text
-buildDeskText (act, inact, color1, color2) current total = "<" `append` before `append` cur `append` after `append` ">"
+buildDeskText (act, inact, color1, color2) current total = T.concat [color2, "<", color2, before, cur, color2, after, color2, ">"]
     where
-        before = T.replicate current (color2 `append` inact)
+        before = T.replicate current inact
         cur = color1 `append` act
-        after = T.replicate (total - current - 1) (color2 `append` inact)
+        after = T.replicate (total - current - 1) inact
 
 
 main :: IO ()
-main = shelly $ silently $ do
-        time <- liftIO getZonedTime
+main = shelly $ tracing False $ forever $ do
+        time <- silently $ liftIO getZonedTime
         let timestring = pack $ case (clockParser $ show time) of
                              Right str -> str
                              Left _ -> ""
@@ -63,29 +64,29 @@ main = shelly $ silently $ do
                            "Discharging" -> "↓"
                            _ -> ""
 
-        volLvl <- run "amixer" ["get", "Master"]
+        volLvl <- silently $ run "amixer" ["get", "Master"]
         -- A bit of a kludge mixing parsec with prelude
         let volLevel = pack $ case (volParser $ Prelude.last $ Prelude.lines $ show volLvl) of
                            Right str -> str
                            Left _ -> show ""
 
-        cpuList <- run "ps" ["-eo", "pcpu"]
+        cpuList <- silently $ run "ps" ["-eo", "pcpu"]
         let cpu = pack $ show $ psOutSum $ strip cpuList
 
-        pmemList <- run "ps" ["-eo", "pmem"]
+        pmemList <- silently $ run "ps" ["-eo", "pmem"]
         let pmem = pack $ show $ psOutSum $ strip pmemList
 
-        curDesktop <- run "xprop" ["-root", "_NET_CURRENT_DESKTOP"]
+        curDesktop <- silently $ run "xprop" ["-root", "_NET_CURRENT_DESKTOP"]
         let currentDesktop = numParser $ unpack $ strip curDesktop
 
-        deskNum <- run "xprop" ["-root", "_NET_NUMBER_OF_DESKTOPS"]
+        deskNum <- silently $ run "xprop" ["-root", "_NET_NUMBER_OF_DESKTOPS"]
         let desktopNumber = numParser $ unpack $ strip deskNum
 
         let color2 = "%{F#ff42413f}"
         let color1 = "%{F#ff8a8987}"
         let deskText = buildDeskText ("↑", "↓", color1, color2) currentDesktop desktopNumber
 
-        let output = [color1, deskText, pack "%{c}", color2, timestring, pack "%{r}"
+        let output = [color2, deskText, pack "%{c}", color1, timestring, pack "%{r}"
                      , color1, pack "CPU: ", color2, cpu, pack "%% "
                      , color1, pack "PMEM: ", color2, pmem, pack "%% "
                      , color1, pack "VOL: ", color2, volLevel, pack "%% "
@@ -93,7 +94,6 @@ main = shelly $ silently $ do
         echo $ T.concat output
 
         sleep 1
-        liftIO main
 
 
 
